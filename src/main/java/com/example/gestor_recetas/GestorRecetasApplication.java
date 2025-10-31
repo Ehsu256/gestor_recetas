07b4d846cca24b0c503ebef2005a554c2c740464
Ehsu256 <estic255@gmail.com>
2025-10-30 20:05:45 -0500

    Primer commit

---
+++ src/main/java/com/example/gestor_recetas/GestorRecetasApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestor_recetas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorRecetasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorRecetasApplication.class, args);
+	}
+
+}
