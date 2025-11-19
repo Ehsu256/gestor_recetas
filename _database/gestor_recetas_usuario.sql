e5b7bc2ccebf0987baba12e3346906eb072b8eda
Ehsu256 <estic255@gmail.com>
2025-11-19 12:52:07 -0500

    Actualización de la base de datos

--- _database/gestor_recetas_usuario.sql
+++ _database/gestor_recetas_usuario.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.42, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
 --
--- Host: localhost    Database: gestor_recetas
+-- Host: 127.0.0.1    Database: gestor_recetas
 -- ------------------------------------------------------
--- Server version	8.0.43
+-- Server version	8.0.22
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -29,7 +29,7 @@ CREATE TABLE `usuario` (
   `redes` varchar(255) DEFAULT NULL,
   `rol` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id_usuario`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,7 +38,7 @@ CREATE TABLE `usuario` (
 
 LOCK TABLES `usuario` WRITE;
 /*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` VALUES (1,'root','admin@gmail.com','','admin');
+INSERT INTO `usuario` VALUES (1,'root','admin@gmail.com','','admin'),(2,'1234','daniel@gmail.com',NULL,'usuario'),(3,'1234','sebas@gmail.com',NULL,'usuario'),(4,'1234','nuevo@gmail.com',NULL,'usuario'),(5,'4321','nuevo2@mail.com',NULL,'usuario');
 /*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -51,4 +51,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-11-13 19:23:30
+-- Dump completed on 2025-11-19 12:51:48
